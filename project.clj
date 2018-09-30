869b03a7f1483d87f4d5cf237043d8cafe487651
Yehonathan Sharvit <viebel@gmail.com>
2018-09-30 16:34:58 +0300

    version 7.6.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject viebel/klipse-clj "7.5.3"
+(defproject viebel/klipse-clj "7.6.0"
   :description "Self-hosted clojurescript library"
   :license "GPL-3.0"
   :url "https://github.com/viebel/klipse-clj"
