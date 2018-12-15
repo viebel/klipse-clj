6b79af95c1727d52c059a1e642909382b35e47ca
Yehonathan Sharvit <viebel@gmail.com>
2018-12-16 01:06:15 +0200

    7.6.8

--- project.clj
+++ project.clj
@@ -1,16 +1,7 @@
-(defproject viebel/klipse-clj "7.6.7"
+(defproject viebel/klipse-clj "7.6.8"
   :description "Self-hosted clojurescript library"
   :license "GPL-3.0"
   :url "https://github.com/viebel/klipse-clj"
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.10.238"]
-                 [org.clojure/tools.reader "1.3.0"]
-                 [org.clojure/core.async "0.4.474"]
-                 [im.chit/purnam "0.5.2"]
-                 [viebel/codemirror-parinfer "0.0.3"]
-                 [cljs-http "0.1.42"]
-                 [viebel/gadjett "0.5.2"]
-                 [com.bhauman/figwheel-main "0.1.9-SNAPSHOT"]]
   :clean-targets ^{:protect false} ["resources/public"
                                     "resources/private"]
   :plugins [[lein-cljsbuild "1.1.5"]
