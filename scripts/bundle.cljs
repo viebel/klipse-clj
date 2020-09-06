2080e86f9f9cbbd041c5fcdfaf3a679ad5a7e893
Yehonathan Sharvit <viebel@gmail.com>
2020-09-06 09:53:12 +0300

    Add a script to generate Klipse bundle for libs

---
+++ scripts/bundle.cljs
@@ -0,0 +1,12 @@
+(ns klipse-bundle.bundle
+  (:require cljs.analyzer.api
+            cljs.analyzer
+            cljs.env
+            cljs.repl
+            clojure.spec.alpha
+            clojure.walk
+            clojure.zip
+            clojure.edn
+            clojure.data
+            clojure.datafy
+            clojure.set))
